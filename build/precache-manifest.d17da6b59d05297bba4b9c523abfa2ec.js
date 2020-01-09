66621c58b889eee4ea61e3eb718d09596d982eba
juliotwo <juliotwo1996@gmail.com>
2020-01-09 00:23:48 -0600

    Add files via upload

---
+++ build/precache-manifest.d17da6b59d05297bba4b9c523abfa2ec.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f8e8951f2e15c6278dddd2e7c773f68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04364999db69b2e94e36",
+    "url": "/static/css/main.5c992196.chunk.css"
+  },
+  {
+    "revision": "7015e80acb5e66a68653",
+    "url": "/static/js/2.0bd0c1df.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.0bd0c1df.chunk.js.LICENSE"
+  },
+  {
+    "revision": "04364999db69b2e94e36",
+    "url": "/static/js/main.789490a2.chunk.js"
+  },
+  {
+    "revision": "d38e4dccdbac9f7062a2",
+    "url": "/static/js/runtime-main.7dba0ef5.js"
+  },
+  {
+    "revision": "f608b49dd99c6c453e568c149afc75e8",
+    "url": "/static/media/catext.f608b49d.svg"
+  },
+  {
+    "revision": "6713b53a1acaacac5c229c1b76b2ae87",
+    "url": "/static/media/credito.6713b53a.svg"
+  },
+  {
+    "revision": "f4fe3786b2741afb4eda6fd264b7a4de",
+    "url": "/static/media/elektra_logo.f4fe3786.svg"
+  },
+  {
+    "revision": "3f85d8035b4ccd91d2a1808dd22b7684",
+    "url": "/static/media/line-awesome.3f85d803.eot"
+  },
+  {
+    "revision": "452a5b42cb4819f09d35bcf6cbdb24c1",
+    "url": "/static/media/line-awesome.452a5b42.woff2"
+  },
+  {
+    "revision": "4d42f5f0c62a8f51e876c14575354a6e",
+    "url": "/static/media/line-awesome.4d42f5f0.ttf"
+  },
+  {
+    "revision": "5207295c5bccd6e7442cfb261446c1f7",
+    "url": "/static/media/line-awesome.5207295c.svg"
+  },
+  {
+    "revision": "8b1290595e57e1d49d95ff3fa1129ecc",
+    "url": "/static/media/line-awesome.8b129059.woff"
+  },
+  {
+    "revision": "cbcb3ab1b2c9ae5d160bc451a278b807",
+    "url": "/static/media/omnipos.cbcb3ab1.svg"
+  },
+  {
+    "revision": "909a43e1158d344126d608007a12cf25",
+    "url": "/static/media/untitled-font-1.909a43e1.svg"
+  },
+  {
+    "revision": "a2db9c694e3cd795fbda6c96eba573a5",
+    "url": "/static/media/untitled-font-1.a2db9c69.eot"
+  },
+  {
+    "revision": "c07f861ddb0eb1a46532d07b715f01f0",
+    "url": "/static/media/untitled-font-1.c07f861d.woff"
+  },
+  {
+    "revision": "fe76b74fa2b5b52a852157c50909405f",
+    "url": "/static/media/untitled-font-1.fe76b74f.ttf"
+  }
+]);
